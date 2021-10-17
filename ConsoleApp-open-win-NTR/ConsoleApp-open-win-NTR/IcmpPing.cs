891c29e98d5198402911ff1b2b8588c3eb4ba231
Alexander Pototsky <silencemyalise@gmail.com>
2021-10-18 01:23:24 +0300

    Build 0.1
    Изменения:
    Первый консольный работающий пинг по циклу.

---
+++ ConsoleApp-open-win-NTR/ConsoleApp-open-win-NTR/IcmpPing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace ConsoleApp_open_win_NTR
+{
+    class IcmpPing
+    {
+        public PingReply IcmpRequest(string hosname)
+        {
+            Ping ping = new Ping();
+            PingReply res = ping.Send(hosname, 1000);
+            return res;
+        }
+
+    }
+}
---
+++ ConsoleApp-open-win-NTR/ConsoleApp-open-win-NTR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_open_win_NTR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
