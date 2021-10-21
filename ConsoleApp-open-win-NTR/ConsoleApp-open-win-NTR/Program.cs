8bc7563dd43e9d6accafed4dd45638c347257d32
Alexander Pototsky <silencemyalise@gmail.com>
2021-10-22 01:46:43 +0300

    Build 0.3
    Изменения:
    Реализовал Readonly переменную и реализовал в коде от греха подальше.
    Сделал в 1 строчку преобразование Списка IPadress в конечный тип нужного для обрабоки LIst.
    Попробовал вывести все на консоль. Ужас. Откатил,

--- ConsoleApp-open-win-NTR/ConsoleApp-open-win-NTR/Program.cs
+++ ConsoleApp-open-win-NTR/ConsoleApp-open-win-NTR/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Threading;
@@ -15,23 +17,20 @@ namespace ConsoleApp_open_win_NTR
             const string host = "vk.com";
             Console.WriteLine("Please stand by");
 
-            var tabletracert = TraceRoute.GetTraceRoute(host);
-            List<string> reTabletracert = new List<string>(); // Делаем свой list что-бы знать длину+ меням тип.
+            List<IPAddress> tabletracert = (List<IPAddress>)TraceRoute.GetTraceRoute(host);
+            List<string> reTabletracert = tabletracert.ConvertAll(s => s.ToString()); // Делаем свой list что-бы знать длинну +меням тип.
 
-            foreach (IPAddress i in tabletracert)
-            {
-                string ip = Convert.ToString(i); // convert type Ipaddres(TraceRoute) for String.
-                reTabletracert.Add(ip); // Add for list ip
-                Console.WriteLine(ip);
-            }
 
             // Create a list for RoundtripTime.
             // Создание списка для RoundtripTime по количеству хопов.
-            var multiDynamicRoundtripTimeList = new List<List<string>> { };
+            List<List<string>> multiDynamicRoundtripTimeList = new List<List<string>> {};
             for (int i = 0; i < reTabletracert.Count; i++)
             {
                 multiDynamicRoundtripTimeList.Add(new List<string> { });
             }
+
+            // Намеренная изоляция в ReadOnly.
+            var multiDynamicRoundtripTimeList_ReadOnly = multiDynamicRoundtripTimeList.AsReadOnly();
 
             // Цикл для постоянного пинга по очереди. async будет потом следующий версиях.
             while (true)
@@ -40,18 +39,19 @@ namespace ConsoleApp_open_win_NTR
                 {
                     IcmpPing ping = new IcmpPing();
                     PingReply result = ping.IcmpRequest(i);
-                    multiDynamicRoundtripTimeList[count].Add($"{result.RoundtripTime}");
 
+                    multiDynamicRoundtripTimeList[count].Add($"{result.RoundtripTime}");
+      
                     Console.Clear(); // Clear CMD
                     Console.WriteLine(result.Address);
                     Console.WriteLine($"Задержка: {result.RoundtripTime}");
                     Console.WriteLine("++++++++++++++++++++++++");
-
+                   
                     // Перебор для вывода list с задержками ping
                     Console.WriteLine("History RoundtripTime: ");
-                    for(int j = 0; j<multiDynamicRoundtripTimeList[count].Count; j++)
+                    for(int j = 0; j < multiDynamicRoundtripTimeList_ReadOnly[count].Count; j++)
                     {
-                        Console.Write($"{multiDynamicRoundtripTimeList[count][j]}ms ");
+                        Console.Write($"{multiDynamicRoundtripTimeList_ReadOnly[count][j]}ms ");
                     }
 
                     count++; // count
